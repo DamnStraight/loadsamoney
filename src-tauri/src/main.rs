501f4dcaaacc2cd88a597a5212b2237c3e1ca1c2
DamnStraight <realdamnstraight@protonmail.com>
2024-06-17 19:56:28 -0400

    Fix for hydration warning, moved themeprovider, more test rust prisma code

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,8 +5,9 @@
 mod db;
 
 use db::*;
+use prisma_client_rust::QueryError;
 use serde::Deserialize;
-use std::sync::Arc;
+use std::{sync::Arc, vec};
 use tauri::State;
 use tauri_specta::ts;
 use specta::{collect_types, Type};
@@ -17,14 +18,15 @@ type DbState<'a> = State<'a, Arc<PrismaClient>>;
 async fn main() {
   let db = PrismaClient::_builder().build().await.unwrap();
   let _ = ts::export(
-    collect_types![add_account, get_account], 
+    collect_types![add_account, get_account, get_all_users], 
     "../types/bindings.ts"
   );
 
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![
       add_account,
-      get_account
+      get_account,
+      get_all_users
     ])
     .manage(Arc::new(db))
     .run(tauri::generate_context!())
@@ -36,6 +38,15 @@ struct CreateAccountData {
   account_type: String,
   name: String,
   amount: f64,
+}
+
+#[tauri::command]
+#[specta::specta]
+async fn get_all_users(db: DbState<'_>) -> Result<Vec<user::Data>, QueryError> {
+  db.user()
+    .find_many(vec![])
+    .exec()
+    .await
 }
 
 #[tauri::command]
